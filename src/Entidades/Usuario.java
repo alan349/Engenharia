1353ee3a042140b9cc121ef406bca826fe8cbde3
alan349 <alan.349@hotmail.com>
2017-03-22 22:05:28 -0300

    Commit Inicial

---
+++ src/Entidades/Usuario.java
@@ -0,0 +1,117 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidades;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+/**
+ *
+ * @author Alan
+ */
+@Entity
+public class Usuario {
+    @Id
+    @Column(nullable = false, unique = true)
+    private Float CPF;
+    @Column(length = 20, nullable = false, unique = true)
+    private String RG;
+    @Column(length = 200, nullable = false)
+    private String nome;
+    @Column(nullable = false, unique = true)
+    private String usuario;
+    @Column(nullable = false)
+    private String senha;
+    @Column(nullable = false)
+    private Integer NP;
+
+    /**
+     * @return the CPF
+     */
+    public Float getCPF() {
+        return CPF;
+    }
+
+    /**
+     * @param CPF the CPF to set
+     */
+    public void setCPF(Float CPF) {
+        this.CPF = CPF;
+    }
+
+    /**
+     * @return the RG
+     */
+    public String getRG() {
+        return RG;
+    }
+
+    /**
+     * @param RG the RG to set
+     */
+    public void setRG(String RG) {
+        this.RG = RG;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the usuario
+     */
+    public String getUsuario() {
+        return usuario;
+    }
+
+    /**
+     * @param usuario the usuario to set
+     */
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    /**
+     * @return the senha
+     */
+    public String getSenha() {
+        return senha;
+    }
+
+    /**
+     * @param senha the senha to set
+     */
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    /**
+     * @return the NP
+     */
+    public Integer getNP() {
+        return NP;
+    }
+
+    /**
+     * @param NP the NP to set
+     */
+    public void setNP(Integer NP) {
+        this.NP = NP;
+    }
+}
+
---
+++ src/Repositorios/UsuarioRepositorio.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Repositorios;
+
+import Entidades.Usuario;
+import java.util.List;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+/**
+ *
+ * @author Danilo Cardoso
+ */
+public class UsuarioRepositorio {
+    public void inserir(Usuario usuario){
+        Session sessao =  Hibernate.NewHibernateUtil.getSessionFactory().openSession();
+        Transaction transacao = sessao.beginTransaction();
+        sessao.save(usuario);
+        transacao.commit();
+        sessao.close();
+    }
+public Usuario buscarPorUsuario(String usuario){
+        System.out.println("2");
+        Session sessao =  Hibernate.NewHibernateUtil.getSessionFactory().openSession();
+        System.out.println("3");
+        Query query = sessao.createQuery("from Usuario where usuario = :usuario");
+        query.setParameter("usuario", usuario);
+        List list = query.list();
+        Usuario user = (Usuario) list.get(0);
+        System.out.println("4");
+        sessao.close();
+        
+        return user;
+    }
+public void excluir(Usuario usuario){
+        Session sessao = Hibernate.NewHibernateUtil.getSessionFactory().openSession();
+        Transaction transacao = sessao.beginTransaction();
+        sessao.delete(usuario);
+        transacao.commit();
+        sessao.close();
+    }
+public void editar(Usuario usuario){
+        Session sessao = Hibernate.NewHibernateUtil.getSessionFactory().openSession();        
+        Transaction transacao = sessao.beginTransaction();
+        sessao.update(usuario);
+        transacao.commit();
+        sessao.close();
+    }
+}
